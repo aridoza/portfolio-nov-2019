e3b4553e8fb2a50faa8cb847eb7c1dc03c9cc46e
aridoza <mendozaarielr@gmail.com>
2019-11-20 11:12:22 -0500

    Updates

---
+++ precache-manifest.10e91572cd20a650cdd880bebfba0189.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6834fc55f89c12f2ba451787e2ec0585",
+    "url": "/portfolio-nov-2019/index.html"
+  },
+  {
+    "revision": "daf436a4bf54dd147b0d",
+    "url": "/portfolio-nov-2019/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "b3c3b219b2a2a484cdac",
+    "url": "/portfolio-nov-2019/static/js/2.d70335c5.chunk.js"
+  },
+  {
+    "revision": "daf436a4bf54dd147b0d",
+    "url": "/portfolio-nov-2019/static/js/main.4f1a2989.chunk.js"
+  },
+  {
+    "revision": "52c12fb08f7ea529ea3d",
+    "url": "/portfolio-nov-2019/static/js/runtime-main.b639b9de.js"
+  }
+]);
